0fd5a8fc2bb718b53d28382e3cc3f18b86347f2b
montasir <montasir1400@gmail.com>
2020-08-19 17:47:25 +0300

    Продолжаем вёрстку личного проекта - доработка

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,9 @@
 var navMain = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-nav__toggle");
+var navToggle = navMain.querySelector(".main-nav__toggle");
 
-navMain.classList.add("main-nav--closed");
+navMain.classList.toggle("main-nav--closed");
 
 navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
+  navMain.classList.toggle("main-nav--closed");
+  navMain.classList.toggle("main-nav--opened");
 });
