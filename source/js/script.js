ce2a859d816ea4d6d0480794b6ecf568f7ed3164
montasir <montasir1400@gmail.com>
2020-08-21 01:27:58 +0300

    улучшение класса comments__arrow

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,9 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = navMain.querySelector(".main-nav__toggle");
-var comments = document.querySelector(".comments");
-var arrowRight = comments.querySelector(".comments__arrow--right");
-var arrowLeft = comments.querySelector(".comments__arrow--left");
 
 navMain.classList.toggle("main-nav--closed");
 
 navToggle.addEventListener("click", function() {
   navMain.classList.toggle("main-nav--closed");
   navMain.classList.toggle("main-nav--opened");
-});
-
-arrowRight.addEventListener("click", function() {
-  // Your code here
-  // console.log("Right");
-});
-
-arrowLeft.addEventListener("click", function() {
-  // Your code here
-  // console.log("Left");
 });
