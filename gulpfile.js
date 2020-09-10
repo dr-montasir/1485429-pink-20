7a62bfc6fcc037e7ff7cb2afacec7006ca48a1a3
montasir <montasir1400@gmail.com>
2020-09-10 05:00:58 +0300

    Сборка завершена

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
+const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -22,9 +23,9 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(csso())
-    .pipe(rename("styles.min.css"))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -35,7 +36,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -89,7 +90,44 @@ const sprite = () => {
   return gulp.src("source/img/**/icon-*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.sprite = sprite;
+
+// Copy
+
+const copy = () => {
+  return gulp
+    .src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.html",
+      "source/*.ico"
+      ], {
+        base: "source",
+      })
+    .pipe(gulp.dest("build")
+  );
+};
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+const build = () => gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+);
+
+exports.build = build;
