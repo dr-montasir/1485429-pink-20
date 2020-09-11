ff45a37ee3b713c24ba9b431027a3a4f6c01600c
montasir <montasir1400@gmail.com>
2020-09-11 22:49:58 +0300

    Б25. style.min.css и style.css

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
